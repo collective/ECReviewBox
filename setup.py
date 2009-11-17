7702d4640576138cd52590b3cf5daf7afc032bd2
St Guenth <stguenth@educomponents.org>
2009-11-17 00:15:23 +0000

    Adds some package metadata to setup.py.
    Specificallly the placeholders for author_mail and url are filled in,
    some classifiers also used for the other eduComponents and the
    dependency on ECAssignmentBox are added.

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ setup(name='Products.ECReviewBox',
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
       author='Michael Piotrowski',
-      author_email='author_mail_placeholder',
-      url='url_placeholder',
+      author_email='mxp@dynalabs.de',
+      url='http://plone.org/products/ecreviewbox',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
@@ -26,6 +28,7 @@ setup(name='Products.ECReviewBox',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'Products.ECAssignmentBox',
       ],
       entry_points="""
       # -*- Entry points: -*-
