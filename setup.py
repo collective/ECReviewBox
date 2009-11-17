157ca03db6027511f256f00f58c2be1d9bfb7867
St Guenth <stguenth@educomponents.org>
2009-11-17 00:15:21 +0000

    Churn: respect 79 character limit.
    It's even in the python coding recommendations. At least paster should
    adhere to that. Some day I should fix the template at least for me.

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(name='Products.ECReviewBox',
       description="The review box component of the eduComponents project.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
